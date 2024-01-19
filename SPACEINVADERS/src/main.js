6ba12a25dc46c652149bb7a2329dbd431c96dc28
julien oving <37832@ma-web.nl>
2024-01-19 14:20:32 +0100

    beansss
    kaboom tutorial

--- SPACEINVADERS/src/main.js
+++ SPACEINVADERS/src/main.js
@@ -1,12 +1,72 @@
 import kaboom from "kaboom"
+import { Scale } from "phaser"
 
-const k = kaboom()
+kaboom()
+loadSprite("bean", "sprites/bean.png")
 
-k.loadSprite("bean", "sprites/bean.png")
+scene("game", () => {
+	let score = 0;
+	const scoreLable = add([
+		text(score),
+		pos(24,24),
+	])
+	onUpdate(() => {
+		score++;
+		scoreLable.text = score;
+	})
+setGravity(1500);
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
+const bean = add([
+	sprite("bean"),
+	pos(80, 40),
+	area(),
+	body(),
 ])
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+onKeyPress("space", () =>{
+	if (bean.isGrounded()){
+		bean.jump();
+	}
+});
+
+const platform = add([
+	rect(width(), 48),
+	pos(0, height() - 48),
+	outline(4),
+	area(),
+	body({isStatic: true}),
+	color(127, 200, 255),
+]);
+function spawnTree(){
+add([
+	rect(48,rand(24, 75)),
+	area(),
+	outline(4),
+	pos(width(), height() - 48),
+	anchor("botleft"),
+	color(255, 180, 255),
+	move(LEFT, 240),
+	"tree"
+]);
+wait(rand(1, 2), () =>{
+	spawnTree();
+});
+};
+bean.onCollide("tree", () =>{
+	addKaboom(bean.pos);
+	shake();
+	go("gameOver");
+})
+spawnTree();
+})
+
+scene("gameOver", () => {
+	add([
+		text("game over"),
+		pos(center()),
+		anchor("center"),
+		scale(4),
+		color(255,0,0)
+	])
+})
+go("game")

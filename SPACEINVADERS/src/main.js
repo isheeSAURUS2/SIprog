191c1a3c283f43aa7a802861eab5243e4954fb7a
julien oving <37832@ma-web.nl>
2024-01-23 12:52:37 +0100

    player
    added playermovement and shooting

--- SPACEINVADERS/src/main.js
+++ SPACEINVADERS/src/main.js
@@ -1,72 +1,67 @@
 import kaboom from "kaboom"
-import { Scale } from "phaser"
-
 kaboom()
-loadSprite("bean", "sprites/bean.png")
+//pre load for sprite and spritesheets
+loadSprite("player", "img/player.png")
+loadSprite("enemyProjectile", "img/enemyProj.png")
+loadSprite("playerProjectile", "img/playerProj.png")
 
+//scenes
 scene("game", () => {
-	let score = 0;
-	const scoreLable = add([
-		text(score),
-		pos(24,24),
-	])
-	onUpdate(() => {
-		score++;
-		scoreLable.text = score;
-	})
-setGravity(1500);
+    setBackground(25,25,25);
+    
+    
+    let speed = 450;
+    //making an object
+    const player = add([
+        //sprite
+        sprite("player"),
+        //scale
+        scale(5),
+        //position
+        pos(885,800),
+        //collision
+        area(),
+        //mass and gravity
+        body(),
+        //health
+        health(10),
+        //tags
+        "player",
+    ])
+    onKeyPress("space", () =>{
+        const PPJ = add([
+            sprite("playerProjectile"),
+            scale(3),
+            area(),
+            body(),
+            pos(player.pos.x + 30, player.pos.y),
+            move(player.pos.y + 10, -700),
+            offscreen({destroy : true}),
+            "PlayerPJ",
+        ])
+        
+    })
+    add([
+        rect(20, height()),
+        color(0,0,0),
+        pos(550, 0),
+        area(),
+        body({isStatic : true}),
+    ])
+    add([
+        rect(20, height()),
+        color(0,0,0),
+        pos(1250, 0),
+        area(),
+        body({isStatic : true}),
+    ])
 
-const bean = add([
-	sprite("bean"),
-	pos(80, 40),
-	area(),
-	body(),
-])
 
-onKeyPress("space", () =>{
-	if (bean.isGrounded()){
-		bean.jump();
-	}
-});
-
-const platform = add([
-	rect(width(), 48),
-	pos(0, height() - 48),
-	outline(4),
-	area(),
-	body({isStatic: true}),
-	color(127, 200, 255),
-]);
-function spawnTree(){
-add([
-	rect(48,rand(24, 75)),
-	area(),
-	outline(4),
-	pos(width(), height() - 48),
-	anchor("botleft"),
-	color(255, 180, 255),
-	move(LEFT, 240),
-	"tree"
-]);
-wait(rand(1, 2), () =>{
-	spawnTree();
-});
-};
-bean.onCollide("tree", () =>{
-	addKaboom(bean.pos);
-	shake();
-	go("gameOver");
-})
-spawnTree();
-})
-
-scene("gameOver", () => {
-	add([
-		text("game over"),
-		pos(center()),
-		anchor("center"),
-		scale(4),
-		color(255,0,0)
-	])
+    onKeyDown("left", () =>{
+        player.move(-speed, 0)
+    })
+    onKeyDown("right", () =>{
+        player.move(speed, 0)
+    })
 })
 go("game")
